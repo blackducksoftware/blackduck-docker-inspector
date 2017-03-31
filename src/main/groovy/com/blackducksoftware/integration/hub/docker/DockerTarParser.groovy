da8dea954acfc98a4079fb51eeddbfeb8fac21f6
jrichard <jrichard@blackducksoftware.com>
2017-03-31 11:50:26 -0400

    Changing tar parser to only extract the /var/lib directory

--- src/main/groovy/com/blackducksoftware/integration/hub/docker/DockerTarParser.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/docker/DockerTarParser.groovy
@@ -12,6 +12,7 @@
 package com.blackducksoftware.integration.hub.docker
 
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.commons.io.FileUtils
 import org.apache.commons.io.IOUtils;
 
 class DockerTarParser {
@@ -20,22 +21,30 @@ class DockerTarParser {
 
     DockerTarResults parseImageTar(File dockerTar){
         List<File> layerTars = extractLayerTars(dockerTar)
-        layerTars.each { layerTar -> parseLayerTarAndExtract(layerTar) }
+        File extractedLayerDirectory = null
+        layerTars.each { layerTar ->
+            if(extractedLayerDirectory == null){
+                extractedLayerDirectory = parseLayerTarAndExtract(layerTar)
+            }
+        }
         null
     }
 
     private List<File> extractLayerTars(File dockerTar){
         List<File> untaredFiles = new ArrayList<>()
-        final File outputDir = new File(workingDirectory, dockerTar.getName());
+        final File outputDir = new File(workingDirectory, dockerTar.getName())
+        if(outputDir.exists()){
+            FileUtils.deleteDirectory(outputDir)
+        }
+        outputDir.mkdirs()
         def tarArchiveInputStream = new TarArchiveInputStream(new FileInputStream(dockerTar))
         try {
             def tarArchiveEntry
             while (null != (tarArchiveEntry = tarArchiveInputStream.getNextTarEntry())) {
-                println tarArchiveEntry.name
                 final File outputFile = new File(outputDir, tarArchiveEntry.getName())
                 if (tarArchiveEntry.isDirectory()) {
                     outputFile.mkdirs()
-                }else if(tarArchiveEntry.name.contains('layer.tar')){
+                } else if(tarArchiveEntry.name.contains('layer.tar')){
                     final OutputStream outputFileStream = new FileOutputStream(outputFile)
                     try{
                         IOUtils.copy(tarArchiveInputStream, outputFileStream)
@@ -51,19 +60,37 @@ class DockerTarParser {
         untaredFiles
     }
 
-    private void parseLayerTarAndExtract(File layerTar){
+    private File parseLayerTarAndExtract(File layerTar){
+        def foundVarLib = false
+        def outputDir = new File(workingDirectory, layerTar.getName())
+        if(outputDir.exists()){
+            FileUtils.deleteDirectory(outputDir)
+        }
         def layerInputStream = new TarArchiveInputStream(new FileInputStream(layerTar))
         try {
             def layerEntry
             while (null != (layerEntry = layerInputStream.getNextTarEntry())) {
-                println layerEntry.name
-                shouldLayerEntryBeExtracted(layerEntry.name)
+                if(layerEntry.name.contains('var/lib')){
+                    foundVarLib = true
+                    final File outputFile = new File(outputDir, layerEntry.getName())
+                    if (layerEntry.isDirectory()) {
+                        outputFile.mkdirs()
+                    } else {
+                        final OutputStream outputFileStream = new FileOutputStream(outputFile)
+                        try{
+                            println layerEntry.name
+                            IOUtils.copy(layerInputStream, outputFileStream)
+                        } finally{
+                            outputFileStream.close()
+                        }
+                    }
+                }
             }
         } finally {
             IOUtils.closeQuietly(layerInputStream)
         }
-    }
-
-    boolean shouldLayerEntryBeExtracted(String entryName){
+        if(foundVarLib){
+            return outputDir
+        }
     }
 }
