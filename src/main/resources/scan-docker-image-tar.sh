4cdd8e93b39e96be49ec1d55d63f16a7af40809a
stevebillings <sbillings@blackducksoftware.com>
2017-04-10 19:20:58 -0400

    IDOCKER-11: added ability to build hub-docker docker image (and a script for running the jar)

---
+++ src/main/resources/scan-docker-image-tar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This script (copied to the Docker container hub-docker will run in)
+# makes it easier to invoke hub-docker (especially from outside the container).
+#
+if [ $# -eq 0 ]
+  then
+    echo "Usage: $0 <path to Docker image tar file>"
+    exit -1
+fi
+cd /opt/blackduck/hub-docker
+java -jar hub-docker-*.jar --docker.tar=$1
