73470d62c9840141598842c8be5903b06ded0246
Steve Billings <sbillings@blackducksoftware.com>
2017-05-17 15:48:57 -0400

    IDOCKER-73: Extractor tests

--- src/test/groovy/com/blackducksoftware/integration/hub/docker/extractor/DpkgExtractorTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/docker/extractor/DpkgExtractorTest.groovy
@@ -1,41 +1,41 @@
-/*
- * Copyright (C) 2017 Black Duck Software Inc.
- * http://www.blackducksoftware.com/
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Black Duck Software ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Black Duck Software.
- */
-package com.blackducksoftware.integration.hub.docker.extractor
+package com.blackducksoftware.integration.hub.docker.extractor;
 
+import static org.junit.Assert.*
+
+import com.blackducksoftware.integration.hub.bdio.simple.BdioWriter
+import com.blackducksoftware.integration.hub.docker.mock.ExecutorMock
+import com.blackducksoftware.integration.hub.docker.OperatingSystemEnum
+import com.blackducksoftware.integration.hub.docker.PackageManagerEnum
+import com.google.gson.Gson
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Test
 import org.apache.commons.io.FileUtils
 
-import com.blackducksoftware.integration.hub.bdio.simple.BdioWriter
-import com.blackducksoftware.integration.hub.docker.OperatingSystemEnum
-import com.blackducksoftware.integration.hub.docker.PackageManagerEnum
-import com.blackducksoftware.integration.hub.docker.mock.ExecutorMock
-import com.google.gson.Gson
+class DpkgExtractorTest {
 
-class ApkExtractorTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
 
-    @Test
-    void testApkFile1() {
-        testApkExtraction('alpine_apk_output_1.txt','testApkBdio1.jsonld')
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+    void testDpkgFile1() {
+        testDpkgExtraction('ubuntu_dpkg_output_1.txt','testDpkgBdio1.jsonld')
     }
 
-    void testApkExtraction(String resourceName, String outputFileName){
+    void testDpkgExtraction(String resourceName, String outputFileName){
         URL url = this.getClass().getResource("/$resourceName")
         File resourceFile = new File(URLDecoder.decode(url.getFile(), 'UTF-8'))
 
-        ApkExtractor extractor = new ApkExtractor()
+        DpkgExtractor extractor = new DpkgExtractor()
         ExecutorMock executor = new ExecutorMock(resourceFile)
         extractor.executor = executor
         def forges = [
-            OperatingSystemEnum.ALPINE.forge
+            OperatingSystemEnum.UBUNTU.forge
         ]
         extractor.initValues(PackageManagerEnum.APK, executor, forges)
 
@@ -47,13 +47,14 @@ class ApkExtractorTest {
         outputFile.getParentFile().mkdirs()
         BdioWriter writer = new BdioWriter(new Gson(), new FileWriter(outputFile))
         ExtractionDetails extractionDetails = new ExtractionDetails()
-        extractionDetails.operatingSystem = OperatingSystemEnum.ALPINE
+        extractionDetails.operatingSystem = OperatingSystemEnum.UBUNTU
         extractionDetails.architecture = 'x86'
         extractor.extract(writer, extractionDetails, "CodeLocationName", "Test", "1")
         writer.close()
 		
-		File file1 = new File("src/test/resources/testApkBdio1.jsonld");
-		File file2 = new File("test/testApkBdio1.jsonld");
+		File file1 = new File("src/test/resources/testDpkgBdio1.jsonld");
+		File file2 = new File("test/testDpkgBdio1.jsonld");
 		boolean isTwoEqual = FileUtils.contentEquals(file1, file2);
     }
+
 }
--- src/test/groovy/com/blackducksoftware/integration/hub/docker/extractor/RpmExtractorTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/docker/extractor/RpmExtractorTest.groovy
@@ -1,41 +1,41 @@
-/*
- * Copyright (C) 2017 Black Duck Software Inc.
- * http://www.blackducksoftware.com/
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Black Duck Software ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Black Duck Software.
- */
-package com.blackducksoftware.integration.hub.docker.extractor
+package com.blackducksoftware.integration.hub.docker.extractor;
 
-import org.junit.Test
-import org.apache.commons.io.FileUtils
+import static org.junit.Assert.*
 
+import com.blackducksoftware.integration.hub.docker.mock.ExecutorMock
+import com.google.gson.Gson
 import com.blackducksoftware.integration.hub.bdio.simple.BdioWriter
 import com.blackducksoftware.integration.hub.docker.OperatingSystemEnum
 import com.blackducksoftware.integration.hub.docker.PackageManagerEnum
-import com.blackducksoftware.integration.hub.docker.mock.ExecutorMock
-import com.google.gson.Gson
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
+import org.apache.commons.io.FileUtils
 
-class ApkExtractorTest {
+class RpmExtractorTest {
 
-    @Test
-    void testApkFile1() {
-        testApkExtraction('alpine_apk_output_1.txt','testApkBdio1.jsonld')
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+    void testRpmFile1() {
+        testRpmExtraction('centos_rpm_output_1.txt','testRpmBdio1.jsonld')
     }
 
-    void testApkExtraction(String resourceName, String outputFileName){
+    void testRpmExtraction(String resourceName, String outputFileName){
         URL url = this.getClass().getResource("/$resourceName")
         File resourceFile = new File(URLDecoder.decode(url.getFile(), 'UTF-8'))
 
-        ApkExtractor extractor = new ApkExtractor()
+        RpmExtractor extractor = new RpmExtractor()
         ExecutorMock executor = new ExecutorMock(resourceFile)
         extractor.executor = executor
         def forges = [
-            OperatingSystemEnum.ALPINE.forge
+            OperatingSystemEnum.CENTOS.forge
         ]
         extractor.initValues(PackageManagerEnum.APK, executor, forges)
 
@@ -47,13 +47,13 @@ class ApkExtractorTest {
         outputFile.getParentFile().mkdirs()
         BdioWriter writer = new BdioWriter(new Gson(), new FileWriter(outputFile))
         ExtractionDetails extractionDetails = new ExtractionDetails()
-        extractionDetails.operatingSystem = OperatingSystemEnum.ALPINE
+        extractionDetails.operatingSystem = OperatingSystemEnum.CENTOS
         extractionDetails.architecture = 'x86'
         extractor.extract(writer, extractionDetails, "CodeLocationName", "Test", "1")
         writer.close()
 		
-		File file1 = new File("src/test/resources/testApkBdio1.jsonld");
-		File file2 = new File("test/testApkBdio1.jsonld");
+		File file1 = new File("src/test/resources/testRpmBdio1.jsonld");
+		File file2 = new File("test/testRpmBdio1.jsonld");
 		boolean isTwoEqual = FileUtils.contentEquals(file1, file2);
     }
 }
