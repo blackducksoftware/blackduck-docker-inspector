75e8532b9e38e59477a49b8d1c622bc396415e75
Steve Billings <sbillings@blackducksoftware.com>
2017-05-17 14:06:42 -0400

    IDOCKER-73: ApplicationTest: testCanProcessInCurrentContainer() refactoring

--- src/test/groovy/com/blackducksoftware/integration/hub/docker/ApplicationTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/docker/ApplicationTest.groovy
@@ -26,8 +26,12 @@ class ApplicationTest {
 
 	@Test
 	public void testCanProcessInCurrentContainer() {
+		doTest("ubuntu")
+	}
+	
+	private void doTest(String targetImageName) {
 		Application app = new Application()
-		app.dockerImage = "ubuntu"
+		app.dockerImage = targetImageName
 		app.dockerImages = new DockerImages()
 		
 		app.hubClient = [
@@ -59,7 +63,7 @@ class ApplicationTest {
 				detectOperatingSystem: {String operatingSystem, File extractedFilesDir -> OperatingSystemEnum.UBUNTU},
 				detectCurrentOperatingSystem: {OperatingSystemEnum.UBUNTU},
 				generateBdioFromImageFilesDir: {List<LayerMapping> mappings, String projectName, String versionName, File dockerTar, File imageFilesDir, OperatingSystemEnum osEnum -> bdioFiles},
-				extractManifestFileContent: {String tarFileName -> "[{\"Config\":\"ebcd9d4fca80e9e8afc525d8a38e7c56825dfb4a220ed77156f9fb13b14d4ab7.json\",\"RepoTags\":[\"ubuntu:latest\"],\"Layers\":[\"68f9022b99e55f4856423cfcdc874e788299fc6147742a5551e10a62e8f2d521/layer.tar\",\"7cc064e7bb40e1237c51dbdf1a2a1d5c533ed67a27936b374adc87886da98a52/layer.tar\",\"09918b7293542c699c6b0c18c7c921472e574e279d42f43af2d70ec9d99dc608/layer.tar\",\"a9ee6ee9019fc117b5290d8f461d0312ff5c06e30b11cc96e43971573918f1b9/layer.tar\",\"9a9fb3763c4d41a028e3427fb412ce68d32a349b7b9fbd8c94eb967117f9b31d/layer.tar\"]}]"},
+				extractManifestFileContent: {String tarFileName -> "[{\"Config\":\"ebcd9d4fca80e9e8afc525d8a38e7c56825dfb4a220ed77156f9fb13b14d4ab7.json\",\"RepoTags\":[\"${targetImageName}\"],\"Layers\":[\"68f9022b99e55f4856423cfcdc874e788299fc6147742a5551e10a62e8f2d521/layer.tar\",\"7cc064e7bb40e1237c51dbdf1a2a1d5c533ed67a27936b374adc87886da98a52/layer.tar\",\"09918b7293542c699c6b0c18c7c921472e574e279d42f43af2d70ec9d99dc608/layer.tar\",\"a9ee6ee9019fc117b5290d8f461d0312ff5c06e30b11cc96e43971573918f1b9/layer.tar\",\"9a9fb3763c4d41a028e3427fb412ce68d32a349b7b9fbd8c94eb967117f9b31d/layer.tar\"]}]"},
 				uploadBdioFiles: {List<File> bdioFilesToUpload -> uploadedBdioFiles = true}
 			] as HubDockerManager
 			
