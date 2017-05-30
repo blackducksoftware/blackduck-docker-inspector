c2d8bb973893ab394b2286da429b2a489eef9b68
Steve Billings <sbillings@blackducksoftware.com>
2017-05-30 11:29:49 -0400

    Test: added printlns for debugging

--- src/test/groovy/com/blackducksoftware/integration/hub/docker/TestUtils.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/docker/TestUtils.groovy
@@ -17,19 +17,23 @@ class TestUtils {
 	}
 	
 	public static boolean contentEquals(File file1, File file2, List<String> exceptLinesContainingThese) {
+		println "Comparing ${file1.getName()} and ${file2.getName()}"
 		List<String> lines1 = file1.readLines()
 		List<String> lines2 = file2.readLines()
 		
 		if (lines1.size() != lines2.size()) {
+			println ("Files' line counts are different")
 			return false
 		}
 		for (int i=0; i < lines1.size(); i++) {
 			String line1 = lines1.get(i)
 			String line2 = lines2.get(i)
+			println "Comparing [${line1}] and [${line2}]"
 			boolean skip = false
 			if (exceptLinesContainingThese != null) {
 				for (String ignoreMe : exceptLinesContainingThese) {
 					if (line1.contains(ignoreMe) && line2.contains(ignoreMe)) {
+						println "Skipping this line"
 						skip = true
 					}
 				}
