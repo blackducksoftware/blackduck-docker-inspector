b180391ab9939b208377224a7aa7129f71c83a88
Steve Billings <sbillings@blackducksoftware.com>
2017-06-01 10:51:50 -0400

    Bump version to 0.0.3-SNAPSHOT

--- src/test/resources/clean_docker.sh
+++ src/test/resources/clean_docker.sh
@@ -3,13 +3,13 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker rmi blackducksoftware/hub-docker-inspector:0.0.2
-docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.2
-docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.2
-
 docker rmi blackducksoftware/hub-docker-inspector:0.0.2-SNAPSHOT
 docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.2-SNAPSHOT
 docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.2-SNAPSHOT
+
+docker rmi blackducksoftware/hub-docker-inspector:0.0.3-SNAPSHOT
+docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.3-SNAPSHOT
+docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.3-SNAPSHOT
 
 docker images
 
