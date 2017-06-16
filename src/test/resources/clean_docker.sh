55aa69c563a445b88c5feb4bb48c593cdebfff73
Steve Billings <sbillings@blackducksoftware.com>
2017-06-16 15:28:59 -0400

    v0.0.4

--- src/test/resources/clean_docker.sh
+++ src/test/resources/clean_docker.sh
@@ -3,21 +3,13 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker rmi blackducksoftware/hub-docker-inspector:0.0.2-SNAPSHOT
-docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.2-SNAPSHOT
-docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.2-SNAPSHOT
-
-docker rmi blackducksoftware/hub-docker-inspector:0.0.3-SNAPSHOT
-docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.3-SNAPSHOT
-docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.3-SNAPSHOT
-
-docker rmi blackducksoftware/hub-docker-inspector:0.0.3
-docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.3
-docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.3
-
 docker rmi blackducksoftware/hub-docker-inspector:0.0.4-SNAPSHOT
 docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.4-SNAPSHOT
 docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.4-SNAPSHOT
+
+docker rmi blackducksoftware/hub-docker-inspector:0.0.4
+docker rmi blackducksoftware/hub-docker-inspector-centos:0.0.4
+docker rmi blackducksoftware/hub-docker-inspector-alpine:0.0.4
 
 docker images
 
