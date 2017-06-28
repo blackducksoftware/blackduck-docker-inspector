7b2c5c6e99c6ca58dcea1f9554d431d65495e33c
Steve Billings <sbillings@blackducksoftware.com>
2017-06-28 12:35:08 -0400

    v0.1.3

--- src/test/resources/clean_docker.sh
+++ src/test/resources/clean_docker.sh
@@ -3,13 +3,13 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker rmi blackducksoftware/hub-docker-inspector:0.1.2
-docker rmi blackducksoftware/hub-docker-inspector-centos:0.1.2
-docker rmi blackducksoftware/hub-docker-inspector-alpine:0.1.2
+docker rmi blackducksoftware/hub-docker-inspector:0.1.4
+docker rmi blackducksoftware/hub-docker-inspector-centos:0.1.4
+docker rmi blackducksoftware/hub-docker-inspector-alpine:0.1.4
 
-docker rmi blackducksoftware/hub-docker-inspector:0.1.2-SNAPSHOT
-docker rmi blackducksoftware/hub-docker-inspector-centos:0.1.2-SNAPSHOT
-docker rmi blackducksoftware/hub-docker-inspector-alpine:0.1.2-SNAPSHOT
+docker rmi blackducksoftware/hub-docker-inspector:0.1.4-SNAPSHOT
+docker rmi blackducksoftware/hub-docker-inspector-centos:0.1.4-SNAPSHOT
+docker rmi blackducksoftware/hub-docker-inspector-alpine:0.1.4-SNAPSHOT
 
 docker rmi blackducksoftware/hub-docker-inspector:0.1.3-SNAPSHOT
 docker rmi blackducksoftware/hub-docker-inspector-centos:0.1.3-SNAPSHOT
