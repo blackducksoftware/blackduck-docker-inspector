2191dbe7a80cc3d7944e3e943d26a2ea3645e490
Steve Billings <sbillings@blackducksoftware.com>
2017-07-05 14:54:42 -0400

    IDOCKER-86: Add application.properties to ubuntu image. refactoring hub-docker-inspector.sh

--- src/main/resources/hub-docker-inspector.sh
+++ src/main/resources/hub-docker-inspector.sh
@@ -20,12 +20,104 @@ function printUsage() {
     echo "configured with your Hub connection details (hub.url, hub.username, and hub.password),"
 	echo "and Docker Hub connection details (docker.registry.username and docker.registry.password)."
 	echo ""
-    exit -1
 }
+
+function preProcessOptions() {
+	for cmdlinearg in "$@"
+	do
+		if [[ $cmdlinearg == --runon=* ]]
+		then
+			runondistro=$(echo $cmdlinearg | cut -d '=' -f 2)
+			echo "Will run on the ${runondistro} image"
+			containername=hub-docker-inspector-${runondistro}
+			imagename=hub-docker-inspector-${runondistro}
+		fi
+		if [[ $cmdlinearg == --spring.config.location=* ]]
+		then
+			propdir=$(echo $cmdlinearg | cut -d '=' -f 2)
+			if [[ $propdir == */ ]]
+			then
+				propdir=$(echo $propdir | rev | cut -c 2- | rev)
+			fi
+		fi
+		if [[ $cmdlinearg == --hub.password=* ]]
+		then
+			hub_password_set_on_cmd_line=true
+		fi
+	done
+}
+
+function collectProxyEnvVars() {
+	if [ -z ${SCAN_CLI_OPTS+x} ]
+	then
+		echo SCAN_CLI_OPTS is not set
+	else
+		echo SCAN_CLI_OPTS is set
+		for cli_opt in $SCAN_CLI_OPTS 
+		do
+			if [[ $cli_opt == -Dhttp*.proxyHost=* ]]
+			then
+				options=( ${options[*]} "--hub.proxy.host=$(echo $cli_opt | cut -d '=' -f 2)" )
+			fi
+			if [[ $cli_opt == -Dhttp*.proxyPort=* ]]
+			then
+				options=( ${options[*]} "--hub.proxy.port=$(echo $cli_opt | cut -d '=' -f 2)" )
+			fi
+			if [[ $cli_opt == -Dhttp*.proxyUser=* ]]
+			then
+				options=( ${options[*]} "--hub.proxy.username=$(echo $cli_opt | cut -d '=' -f 2)" )
+			fi
+			if [[ $cli_opt == -Dhttp*.proxyPassword=* ]]
+			then
+				options=( ${options[*]} "--hub.proxy.password=$(echo $cli_opt | cut -d '=' -f 2)" )
+			fi
+		done
+	fi
+}
+
+function checkForPassword() {
+	if [ $hub_password_set_on_cmd_line = true -o -z "${BD_HUB_PASSWORD}" ]
+	then
+   	    echo Environment variable BD_HUB_PASSWORD is not set or is being overridden on the command line
+	else
+        echo BD_HUB_PASSWORD is set
+	fi
+}
+
+function startContainer() {
+	if [ $(docker ps |grep "${containername}\$" | wc -l) -gt 0 ]
+	then
+		echo ${containername} container is already running
+	else
+		echo ${containername} container is not running
+		docker rm ${containername} 2> /dev/null
+		echo "Pulling/running hub-docker-inspector Docker image"
+		docker run --name ${containername} -it -d --privileged blackducksoftware/${imagename}:@VERSION@ /bin/bash
+	fi
+}
+
+function installPropertiesFile() {
+	if [ -f ${propfile} ]
+	then
+		echo "Found ${propfile}"
+		docker cp ${propfile} ${containername}:/opt/blackduck/hub-docker-inspector/config
+	else
+		echo "File ${propfile} not found."
+		echo "Without this file, you will have to set all required properties via the command line."
+		docker exec ${containername} rm -f /opt/blackduck/hub-docker-inspector/config/application.properties
+	fi
+}
+
+##################
+# Start script
+##################
+
+echo "Start of script"
 
 if [ $# -lt 1 ]
 then
     printUsage
+    exit -1
 fi
 
 if [ \( $1 = -v \) -o \( $1 = --version \) ]
@@ -37,97 +129,24 @@ fi
 if [ \( $1 = -h \) -o \( $1 = --help \) ]
 then
     printUsage
+    exit -1
 fi
 
 containername=hub-docker-inspector
 imagename=hub-docker-inspector
 propdir=.
 hub_password_set_on_cmd_line=false
-
-for cmdlinearg in "$@"
-do
-	if [[ $cmdlinearg == --runon=* ]]
-	then
-		runondistro=$(echo $cmdlinearg | cut -d '=' -f 2)
-		echo "Will run on the ${runondistro} image"
-		containername=hub-docker-inspector-${runondistro}
-		imagename=hub-docker-inspector-${runondistro}
-	fi
-	if [[ $cmdlinearg == --spring.config.location=* ]]
-	then
-		propdir=$(echo $cmdlinearg | cut -d '=' -f 2)
-		if [[ $propdir == */ ]]
-		then
-			propdir=$(echo $propdir | rev | cut -c 2- | rev)
-		fi
-	fi
-	if [[ $cmdlinearg == --hub.password=* ]]
-	then
-		hub_password_set_on_cmd_line=true
-	fi
-done
-
+preProcessOptions "$@"
 propfile=${propdir}/application.properties
 echo "Properties file: ${propfile}"
 
 options=( "$@" )
 image=${options[${#options[@]}-1]}
 unset "options[${#options[@]}-1]"
-
-# Collect proxy env vars
-if [ -z ${SCAN_CLI_OPTS+x} ]
-then
-	echo SCAN_CLI_OPTS is not set
-else
-	echo SCAN_CLI_OPTS is set
-	for cli_opt in $SCAN_CLI_OPTS 
-	do
-		if [[ $cli_opt == -Dhttp*.proxyHost=* ]]
-		then
-			options=( ${options[*]} "--hub.proxy.host=$(echo $cli_opt | cut -d '=' -f 2)" )
-		fi
-		if [[ $cli_opt == -Dhttp*.proxyPort=* ]]
-		then
-			options=( ${options[*]} "--hub.proxy.port=$(echo $cli_opt | cut -d '=' -f 2)" )
-		fi
-		if [[ $cli_opt == -Dhttp*.proxyUser=* ]]
-		then
-			options=( ${options[*]} "--hub.proxy.username=$(echo $cli_opt | cut -d '=' -f 2)" )
-		fi
-		if [[ $cli_opt == -Dhttp*.proxyPassword=* ]]
-		then
-			options=( ${options[*]} "--hub.proxy.password=$(echo $cli_opt | cut -d '=' -f 2)" )
-		fi
-	done
-fi
-
-if [ $hub_password_set_on_cmd_line = true -o -z "${BD_HUB_PASSWORD}" ]
-then
-        echo Environment variable BD_HUB_PASSWORD is not set or is being overridden on the command line
-else
-        echo BD_HUB_PASSWORD is set
-fi
-
-if [ $(docker ps |grep "${containername}\$" | wc -l) -gt 0 ]
-then
-	echo ${containername} container is already running
-else
-	echo ${containername} container is not running
-	docker rm ${containername} 2> /dev/null
-	echo "Pulling/running hub-docker-inspector Docker image"
-	docker run --name ${containername} -it -d --privileged blackducksoftware/${imagename}:@VERSION@ /bin/bash
-fi
-
-if [ -f ${propfile} ]
-then
-	echo "Found ${propfile}"
-	docker cp ${propfile} ${containername}:/opt/blackduck/hub-docker-inspector/config
-else
-	echo "File ${propfile} not found."
-	echo "Without this file, you will have to set all required properties via the command line."
-	docker exec ${containername} rm -f /opt/blackduck/hub-docker-inspector/config/application.properties
-fi
-
+collectProxyEnvVars
+checkForPassword
+startContainer
+installPropertiesFile
 
 if [[ "$image" == *.tar ]]
 then
