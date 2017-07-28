b42bd11ef08883be83ff0e7c28c128ec95e7fd53
Steve Billings <sbillings@blackducksoftware.com>
2017-07-28 12:20:04 -0400

    IDOCKER-164: test fixes

--- src/test/groovy/com/blackducksoftware/integration/hub/docker/EndToEndTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/docker/EndToEndTest.groovy
@@ -3,6 +3,7 @@ package com.blackducksoftware.integration.hub.docker;
 import static org.junit.Assert.*
 
 import java.lang.ProcessBuilder.Redirect
+import java.nio.file.Files
 import java.util.concurrent.TimeUnit
 
 import org.junit.AfterClass
@@ -24,6 +25,12 @@ class EndToEndTest {
 		test("centos", "7.3.1611", "var_lib_rpm")
 	}
 	private void test(String image, String tag, String pkgMgrPathString) {
+		println "Running end to end test on ${image}:${tag}"
+
+		File expectedBdio = new File("src/test/resources/bdio/${image}_${pkgMgrPathString}_${image}_${tag}_bdio.jsonld")
+		File actualBdio = new File("test/output/${image}_${pkgMgrPathString}_${image}_${tag}_bdio.jsonld")
+		Files.deleteIfExists(actualBdio.toPath())
+
 		ProcessBuilder pb =
 				new ProcessBuilder("build/hub-docker-inspector.sh",
 				"--logging.level.com.blackducksoftware=INFO",
@@ -40,8 +47,6 @@ class EndToEndTest {
 		assertTrue(finished)
 		println "hub-docker-inspector done; verifying results..."
 
-		File expectedBdio = new File("src/test/resources/bdio/${image}_${pkgMgrPathString}_${image}_${tag}_bdio.jsonld")
-		File actualBdio = new File("test/output/${image}_${pkgMgrPathString}_${image}_${tag}_bdio.jsonld")
 		List<String> exceptLinesContainingThese = new ArrayList<>()
 		exceptLinesContainingThese.add("\"@id\":")
 		boolean outputBdioIsGood = TestUtils.contentEquals(expectedBdio, actualBdio, exceptLinesContainingThese)
