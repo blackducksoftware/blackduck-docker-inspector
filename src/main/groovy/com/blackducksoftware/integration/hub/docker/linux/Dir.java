412fe0e581e3dca4d20b53cae97411f04fe7e94c
Steve Billings <sbillings@blackducksoftware.com>
2017-08-02 13:19:52 -0400

    IDOCKER-171: findFileWithName(): Use File.findFiles()

--- src/main/groovy/com/blackducksoftware/integration/hub/docker/linux/Dir.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/docker/linux/Dir.java
@@ -1,32 +1,26 @@
 package com.blackducksoftware.integration.hub.docker.linux;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FilenameFilter;
+import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Dir {
     private static final Logger logger = LoggerFactory.getLogger(Dir.class);
 
-    // TODO Implement this using File.listFiles(filter)
     public static List<File> findFileWithName(final File dirFile, final String name) {
-        logger.info(String.format("****** Looking in %s for %s", dirFile.getAbsolutePath(), name));
-        final List<File> files = new ArrayList<>();
-        if (StringUtils.compare(dirFile.getName(), name) == 0) {
-            logger.trace("File Name ${name} found ${dirFile.getAbsolutePath()}");
-            files.add(dirFile);
-        } else if (dirFile.isDirectory()) {
+        logger.info(String.format("======== Looking in %s for %s", dirFile.getAbsolutePath(), name));
 
-            for (final File subFile : dirFile.listFiles()) {
-                final List<File> foundFile = findFileWithName(subFile, name);
-                if (foundFile != null) {
-                    files.addAll(foundFile);
-                }
+        final File[] fileArray = dirFile.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(final File dir, final String name) {
+                return name.matches("^" + name + "$");
             }
-        }
-        return files;
+        });
+        final List<File> fileList = Arrays.asList(fileArray);
+        return fileList;
     }
 }
