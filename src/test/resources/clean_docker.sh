f346769ea133a152d98668109d85f8655a1e6613
Steve Billings <sbillings@blackducksoftware.com>
2017-10-05 13:31:36 -0400

    3.0.0

--- src/test/resources/clean_docker.sh
+++ src/test/resources/clean_docker.sh
@@ -6,9 +6,17 @@ docker rm $(docker ps -a -q)
 docker rmi blackducksoftware/centos_minus_vim_plus_bacula:1.0
 docker rmi blackducksoftware/whiteouttest:1.0
 
+docker rmi blackducksoftware/hub-docker-inspector-ubuntu:3.0.1-SNAPSHOT
+docker rmi blackducksoftware/hub-docker-inspector-centos:3.0.1-SNAPSHOT
+docker rmi blackducksoftware/hub-docker-inspector-alpine:3.0.1-SNAPSHOT
+
 docker rmi blackducksoftware/hub-docker-inspector-ubuntu:3.0.0
 docker rmi blackducksoftware/hub-docker-inspector-centos:3.0.0
 docker rmi blackducksoftware/hub-docker-inspector-alpine:3.0.0
+
+docker rmi blackducksoftware/hub-docker-inspector-ubuntu:3.0.0-RC3
+docker rmi blackducksoftware/hub-docker-inspector-centos:3.0.0-RC3
+docker rmi blackducksoftware/hub-docker-inspector-alpine:3.0.0-RC3
 
 docker rmi blackducksoftware/hub-docker-inspector-ubuntu:3.0.0-RC2
 docker rmi blackducksoftware/hub-docker-inspector-centos:3.0.0-RC2
