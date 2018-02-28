05ebd49d3affb527aff03f5aeace3c224dbf42ab
Steve Billings <sbillings@blackducksoftware.com>
2018-02-27 20:07:30 -0500

    ITs: JAVA_HOME reporting

---
+++ src/integration-test/groovy/com/blackducksoftware/integration/hub/docker/dockerinspector/CmdExecutor.java
@@ -0,0 +1,49 @@
+package com.blackducksoftware.integration.hub.docker.dockerinspector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import com.blackducksoftware.integration.exception.IntegrationException;
+
+public class CmdExecutor {
+    public static String execCmd(final String cmd, final long timeoutSeconds, final Map<String, String> env) throws IOException, InterruptedException, IntegrationException {
+        System.out.println(String.format("Executing: %s", cmd));
+        final ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
+        pb.redirectOutput(Redirect.PIPE);
+        pb.redirectError(Redirect.PIPE);
+        pb.environment().put("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin");
+        if (env != null) {
+            pb.environment().putAll(env);
+        }
+        final Process p = pb.start();
+        final boolean finished = p.waitFor(timeoutSeconds, TimeUnit.SECONDS);
+        if (!finished) {
+            throw new InterruptedException("Command timed out");
+        }
+        final int retCode = p.exitValue();
+        if (retCode != 0) {
+            final String stderr = toString(p.getErrorStream());
+            System.out.println(String.format("%s: stderr: %s", cmd, stderr));
+            throw new IntegrationException(String.format("Command failed: %s", stderr));
+        }
+        return toString(p.getInputStream());
+    }
+
+    private static String toString(final InputStream is) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        final StringBuilder builder = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            if (builder.length() > 0) {
+                builder.append("\n");
+            }
+            builder.append(line);
+        }
+        return builder.toString();
+    }
+}
