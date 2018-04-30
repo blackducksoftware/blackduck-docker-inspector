471eec26a4e96ff76422d393cab88de94086a349
Steve Billings <sbillings@blackducksoftware.com>
2018-04-30 19:02:37 -0400

    UsageFormatterTest -> java

--- src/test/groovy/com/blackducksoftware/integration/hub/docker/dockerinspector/ProgramPathsTest.java
+++ src/test/groovy/com/blackducksoftware/integration/hub/docker/dockerinspector/ProgramPathsTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -18,10 +16,8 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.blackducksoftware.integration.hub.docker.dockerinspector.config.Config;
-import com.blackducksoftware.integration.hub.docker.dockerinspector.config.DockerInspectorOption;
 import com.blackducksoftware.integration.hub.docker.dockerinspector.config.ProgramPaths;
 import com.blackducksoftware.integration.hub.docker.imageinspector.TestUtils;
-import com.blackducksoftware.integration.hub.imageinspector.name.Names;
 
 @RunWith(SpringRunner.class)
 public class ProgramPathsTest {
@@ -51,25 +47,6 @@ public class ProgramPathsTest {
     }
 
     private void doTest(final String jarFileName, final boolean prefixCodeLocationName) throws IllegalArgumentException, IllegalAccessException, IOException {
-        String prefix = "";
-        if (prefixCodeLocationName) {
-            prefix = "xyz";
-        }
-
-        final List<DockerInspectorOption> configOptions = new ArrayList<>();
-        configOptions.add(new DockerInspectorOption("hub.url", "hubUrl", "testHubUrl", "Hub URL", String.class, "", "public", false));
-        BDDMockito.given(config.getPublicConfigOptions()).willReturn(configOptions);
-        BDDMockito.given(config.isOnHost()).willReturn(true);
-        BDDMockito.given(config.isUploadBdio()).willReturn(true);
-        BDDMockito.given(config.getLinuxDistro()).willReturn("");
-        BDDMockito.given(config.getDockerTar()).willReturn("");
-        BDDMockito.given(config.getDockerImage()).willReturn("");
-        BDDMockito.given(config.getDockerImageId()).willReturn("");
-        BDDMockito.given(config.getDockerImageRepo()).willReturn("");
-        BDDMockito.given(config.getDockerImageTag()).willReturn("");
-        BDDMockito.given(config.getHubUrl()).willReturn("");
-        BDDMockito.given(config.getHubCodelocationPrefix()).willReturn(prefix);
-
         final File installDir = TestUtils.createTempDirectory();
         String installDirPath = installDir.getAbsolutePath();
         if (!installDirPath.endsWith("/")) {
@@ -77,20 +54,12 @@ public class ProgramPathsTest {
         }
         BDDMockito.given(config.getWorkingDirPath()).willReturn(installDirPath);
 
-        final File jarFile = new File(installDir, "hub-docker-1.0.0.jar");
-        jarFile.createNewFile();
+        programPaths.init();
 
-        programPaths.init(); // TODO why do I need this?
         assertTrue(programPaths.getHubDockerConfigDirPathHost().startsWith(String.format("%sconfig_", installDirPath)));
         assertTrue(programPaths.getHubDockerConfigFilePathHost().startsWith(String.format("%sconfig_", installDirPath)));
         assertTrue(programPaths.getHubDockerConfigFilePathHost().endsWith(String.format("/application.properties")));
         assertTrue(programPaths.getHubDockerTargetDirPathHost().startsWith(String.format("%starget_", installDirPath)));
         assertEquals(String.format("%s", installDirPath), programPaths.getHubDockerPgmDirPathHost());
-
-        if (prefixCodeLocationName) {
-            assertEquals(String.format("%s_imageName_imageTag_pkgMgrFilePath_pkgMgrName", prefix), Names.getCodeLocationName(prefix, "imageName", "imageTag", "pkgMgrFilePath", "pkgMgrName"));
-        } else {
-            assertEquals("imageName_imageTag_pkgMgrFilePath_pkgMgrName", Names.getCodeLocationName(null, "imageName", "imageTag", "pkgMgrFilePath", "pkgMgrName"));
-        }
     }
 }
---
+++ src/test/groovy/com/blackducksoftware/integration/hub/docker/dockerinspector/help/formatter/UsageFormatterTest.java
@@ -0,0 +1,54 @@
+package com.blackducksoftware.integration.hub.docker.dockerinspector.help.formatter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.blackducksoftware.integration.hub.docker.dockerinspector.config.Config;
+import com.blackducksoftware.integration.hub.docker.dockerinspector.config.DockerInspectorOption;
+
+@RunWith(SpringRunner.class)
+public class UsageFormatterTest {
+
+    @InjectMocks
+    private UsageFormatter usageFormatter;
+
+    @Mock
+    private Config config;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Test
+    public void test() throws IllegalArgumentException, IllegalAccessException, IOException {
+        final List<DockerInspectorOption> configOptions = new ArrayList<>();
+        configOptions.add(new DockerInspectorOption("hub.url", "hubUrl", "testHubUrl", "Hub URL", String.class, "", "public", false));
+        BDDMockito.given(config.getPublicConfigOptions()).willReturn(configOptions);
+
+        final List<String> usageStrings = usageFormatter.getStringList();
+        assertTrue(usageStrings.size() >= 16);
+        assertEquals("Usage: hub-docker-inspector.sh <options>", usageStrings.get(0));
+        assertEquals("options: any supported property can be set by adding to the command line", usageStrings.get(1));
+        final String usageString = StringUtils.join(usageStrings, ";");
+        assertTrue(usageString.contains("hub.url [String]: Hub URL"));
+    }
+
+}
