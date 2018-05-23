06a61f4a786958c2c45ed2d183a9e30c5340cdba
Steve Billings <sbillings@blackducksoftware.com>
2018-05-23 15:05:43 -0400

    fix copy of output to user output dir

--- src/main/java/com/blackducksoftware/integration/hub/docker/dockerinspector/restclient/RestClientInspector.java
+++ src/main/java/com/blackducksoftware/integration/hub/docker/dockerinspector/restclient/RestClientInspector.java
@@ -91,7 +91,14 @@ public class RestClientInspector implements Inspector {
                 logger.info(String.format("Writing BDIO to %s", outputBdioFile.getAbsolutePath()));
                 FileUtils.write(outputBdioFile, bdioString, StandardCharsets.UTF_8);
 
-                final File localPathToContainerOutputDir = new File(config.getSharedDirPathLocal(), "output");
+                // TODO this method of getting localPathToContainerOutputDir is OK for orchestration,
+                // not for cmd-line-util; Changing it from:
+                // new File(config.getSharedDirPathLocal(), "output")
+                // to:
+                // new File(programPaths.getHubDockerOutputPathHost());
+                // Which may break the hub-detect-ws scenario
+                final File localPathToContainerOutputDir = new File(programPaths.getHubDockerOutputPathHost());
+
                 final File localPathToContainerFileSytemFile = new File(localPathToContainerOutputDir, containerFileSystemFilename);
                 final File userContainerFileSytemFile = new File(userOutputDir, containerFileSystemFilename);
                 logger.debug(String.format("Copying %s to %s", localPathToContainerFileSytemFile.getAbsolutePath(), userContainerFileSytemFile.getAbsolutePath()));
