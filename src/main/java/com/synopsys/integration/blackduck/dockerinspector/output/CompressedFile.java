057860ed8d7144b3aac9071da1143ff462e0cf83
Steve Billings <sbillings@blackducksoftware.com>
2019-05-30 13:13:14 -0400

    feat: added methods to unTar and gunZip

--- src/main/java/com/synopsys/integration/blackduck/dockerinspector/output/CompressedFile.java
+++ src/main/java/com/synopsys/integration/blackduck/dockerinspector/output/CompressedFile.java
@@ -8,13 +8,35 @@ import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CompressedFile {
     private static final Logger logger = LoggerFactory.getLogger(CompressedFile.class);
 
-    public static void unpackTarGz(final File fileToUnpack, final File destinationDir) {
+    public static void gunZipUnTarFile(final File tarGzFile, final File tempTarFile, final File destinationDir) throws IOException {
+        gunZipFile(tarGzFile, tempTarFile);
+        unTarFile(tempTarFile, destinationDir);
+    }
+
+    public static void unTarFile(final File tarFile, final File destinationDir) throws IOException {
+        try (final TarArchiveInputStream fin = new TarArchiveInputStream(new FileInputStream(tarFile))) {
+            TarArchiveEntry entry;
+            while ((entry = fin.getNextTarEntry()) != null) {
+                if (entry.isDirectory()) {
+                    continue;
+                }
+                final File curfile = new File(destinationDir, entry.getName());
+                final File parent = curfile.getParentFile();
+                if (!parent.exists()) {
+                    parent.mkdirs();
+                }
+                IOUtils.copy(fin, new FileOutputStream(curfile));
+            }
+        }
 
     }
 
--- src/test/java/com/synopsys/integration/blackduck/dockerinspector/output/CompressedFileTest.java
+++ src/test/java/com/synopsys/integration/blackduck/dockerinspector/output/CompressedFileTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,9 @@ import org.junit.jupiter.api.Test;
 public class CompressedFileTest {
     private static File fileToCompress;
     private static File compressedFile;
+    private static File destinationDir;
+    private static File testTarGzDir;
+    private static File tempTarFile;
 
     @BeforeAll
     public static void setUp() throws IOException {
@@ -22,29 +26,60 @@ public class CompressedFileTest {
         compressedFile.delete();
         fileToCompress.createNewFile();
 
+        destinationDir = new File("test/output/testTarContents");
+        FileUtils.deleteDirectory(destinationDir);
+
+        testTarGzDir = new File("test/output/testTarGzDir");
+        FileUtils.deleteDirectory(testTarGzDir);
+        testTarGzDir.mkdirs();
+
+        tempTarFile = new File("test/output/testTempTar.tar");
     }
 
     @AfterAll
     public static void tearDown() {
         fileToCompress.delete();
         compressedFile.delete();
+        try {
+            FileUtils.deleteDirectory(destinationDir);
+        } catch (IOException e) {
+        }
+        try {
+            FileUtils.deleteDirectory(testTarGzDir);
+        } catch (IOException e) {
+        }
+        tempTarFile.delete();
     }
 
     @Test
-    public void testGunZip() throws IOException {
-        final File fileToUnpack = new File("src/test/resources/test.tar.gz");
+    public void testGunZipFile() throws IOException {
+        final File fileToUnZip = new File("src/test/resources/test1.tar.gz");
         final File unCompressedFile = new File("test/output/test.tar");
-        CompressedFile.gunZipFile(fileToUnpack, unCompressedFile);
+        CompressedFile.gunZipFile(fileToUnZip, unCompressedFile);
         assertTrue(unCompressedFile.exists());
     }
 
-    // TODO this will end up beiung redundant
     @Test
-    public void testUnpackTarGz() throws IOException {
-        final File fileToUnpack = new File("src/test/resources/test.tar.gz");
-        final File destinationDir = new File("test/output/testTarGzDir");
-        CompressedFile.unpackTarGz(fileToUnpack, destinationDir);
-        final File unpackedFile = new File("test/output/testTarGzDir/test.txt");
+    public void testUnTarFile() throws IOException {
+        final File fileToUnTar = new File("src/test/resources/test2.tar");
+
+        CompressedFile.unTarFile(fileToUnTar, destinationDir);
+
+        final File expectedFile1 = new File(destinationDir, "test2.txt");
+        assertTrue(expectedFile1.exists());
+
+        final File expectedFile2 = new File(destinationDir, "subdir/test3.txt");
+        assertTrue(expectedFile2.exists());
+    }
+
+    @Test
+    public void testGunZipUnTarFile() throws IOException {
+        final File fileToUnpack = new File("src/test/resources/test1.tar.gz");
+        assertTrue(fileToUnpack.exists());
+        assertFalse(tempTarFile.exists());
+
+        CompressedFile.gunZipUnTarFile(fileToUnpack, tempTarFile, testTarGzDir);
+        final File unpackedFile = new File("test/output/testTarGzDir/test1.txt");
         assertTrue(unpackedFile.exists());
     }
 
