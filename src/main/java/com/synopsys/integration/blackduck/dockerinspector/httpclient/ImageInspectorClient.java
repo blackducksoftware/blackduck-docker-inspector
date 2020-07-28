01a462c7fd87a799727479720cd93f18d01f682f
Steve Billings <sbillings@blackducksoftware.com>
2020-07-28 14:57:49 -0400

    fix: using existing containers: if target file not already in shared dir, copy it there; fixes IDOCKER-706

--- src/main/java/com/synopsys/integration/blackduck/dockerinspector/httpclient/ImageInspectorClient.java
+++ src/main/java/com/synopsys/integration/blackduck/dockerinspector/httpclient/ImageInspectorClient.java
@@ -22,21 +22,57 @@
  */
 package com.synopsys.integration.blackduck.dockerinspector.httpclient;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.nio.file.Path;
 
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.synopsys.integration.blackduck.dockerinspector.config.Config;
+import com.synopsys.integration.blackduck.dockerinspector.config.ProgramPaths;
 import com.synopsys.integration.blackduck.dockerinspector.output.ImageTarWrapper;
 import com.synopsys.integration.exception.IntegrationException;
 
-public interface ImageInspectorClient {
+public abstract class ImageInspectorClient {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    ImageTarWrapper copyTarfileToSharedDir(final ImageTarWrapper givenDockerTarfile) throws IOException;
+    public ImageTarWrapper copyTarfileToSharedDir(Config config, ProgramPaths programPaths, ImageTarWrapper givenDockerTarfile) throws IOException {
+        if (fileIsInsideDir(new File(config.getSharedDirPathLocal()), givenDockerTarfile.getFile())) {
+            logger.debug(String.format("File %s is already inside shared dir %s; leaving it there",
+                givenDockerTarfile.getFile().getCanonicalPath(),
+                config.getSharedDirPathLocal()));
+            return givenDockerTarfile;
+        }
+        // Copy the tarfile to the shared/target dir
+        logger.debug(String.format("File %s is not inside shared dir %s; copying it there",
+            givenDockerTarfile.getFile().getCanonicalPath(),
+            config.getSharedDirPathLocal()));
+        File finalDockerTarfile = new File(programPaths.getDockerInspectorTargetDirPath(), givenDockerTarfile.getFile().getName());
+        logger.debug(String.format("Required docker tarfile location: %s", finalDockerTarfile.getCanonicalPath()));
+        if (!finalDockerTarfile.getCanonicalPath().equals(givenDockerTarfile.getFile().getCanonicalPath())) {
+            logger.debug(String.format("Copying %s to %s", givenDockerTarfile.getFile().getCanonicalPath(), finalDockerTarfile.getCanonicalPath()));
+            FileUtils.copyFile(givenDockerTarfile.getFile(), finalDockerTarfile);
+        }
+        logger.debug(String.format("Final docker tar file path: %s", finalDockerTarfile.getCanonicalPath()));
+        return new ImageTarWrapper(finalDockerTarfile, givenDockerTarfile.getImageRepo(), givenDockerTarfile.getImageTag());
+    }
 
-    String getBdio(String hostPathToTarFile, String containerPathToInputDockerTarfile, String givenImageRepo, String givenImageTag,
+    private boolean fileIsInsideDir(File dir, File file) {
+        Path sharedDirPathObj = dir.toPath();
+        Path givenFilePathObj = file.toPath();
+        if (givenFilePathObj.startsWith(sharedDirPathObj)) {
+            return true;
+        }
+        return false;
+    }
+
+    public abstract String getBdio(String hostPathToTarFile, String containerPathToInputDockerTarfile, String givenImageRepo, String givenImageTag,
         String containerPathToOutputFileSystemFile, String containerFileSystemExcludedPaths,
-        final boolean organizeComponentsByLayer, final boolean includeRemovedComponents, final boolean cleanup,
-        final String platformTopLayerId, final String targetLinuxDistro)
+        boolean organizeComponentsByLayer, boolean includeRemovedComponents, boolean cleanup,
+        String platformTopLayerId, String targetLinuxDistro)
         throws IntegrationException, IOException, InterruptedException;
 
-    boolean isApplicable();
+    public abstract boolean isApplicable();
 }
