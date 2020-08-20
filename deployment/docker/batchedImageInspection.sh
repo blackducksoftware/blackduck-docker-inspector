9a807faa1422687886d956536a0994b9e985be8b
Steve Billings <sbillings@blackducksoftware.com>
2020-08-20 15:53:40 -0400

    fix: improved the docker batchedImageInspection sample

--- deployment/docker/batchedImageInspection.sh
+++ deployment/docker/batchedImageInspection.sh
@@ -11,66 +11,103 @@
 # ==> Adjust the value of localSharedDirPath
 # This path cannot have any symbolic links in it
 ####################################################################
-localSharedDirPath=/blackduck/shared
-imageInspectorVersion=4.4.0
-blackDuckUrl=https://blackduck.yourdomain.com
+localSharedDirPath=/pathtoanemptydir
+imageInspectorVersion=5.0.1
+blackDuckUrl=https://yourblackduckserver.com
 blackDuckUsername=yourblackduckusername
 blackDuckPassword=yourblackduckpassword
-projectName=TestProject
 
 ####################################################################
-# This script will start the imageinspector alpine service (if
-# it's not already running), and send it 3 requests to inspect
-# alpine:latest
+# This script will start each imageinspector service (if
+# it's not already running), and send 3 requests to inspect
+# three images: alpine:latest, centos:latest, ubuntu:latest.
 #
-# This script will leave the alpine imageinspector service running
-# (and will re-use it on subsequent runs)
+# This script will leave the imageinspector services running
+# (and will re-use them on subsequent runs)
 # For troubleshooting, you might need to do a "docker logs" on
 # the imageinspector service container.
-#
-# To expand this to cover all linux distros, you would need two more "docker run" commands to start two more services (centos and ubuntu)
-# on two other ports. All three "docker run" commands will have all 3 port numbers in them, so every service knows how to find
-# every other service.
-# The alpine service would then be able to redirect requests to those other services when necessary.
 ####################################################################
 
 mkdir -p ${localSharedDirPath}/target
 
+##############################
+# Start the imageinspector services if not already running
+##############################
 alpineServiceIsUp=false
 
 successMsgCount=$(curl http://localhost:9000/health | grep "\"status\":\"UP\"" | wc -l)
 if [ "$successMsgCount" -eq "1" ]; then
-	echo "The service is up"
+	echo "The alpine service is up"
 	alpineServiceIsUp=true
 else
 	# Start the image inspector service for alpine on port 9000
 	docker run -d --user 1001 -p 9000:8081 --label "app=blackduck-imageinspector" --label="os=ALPINE" -v ${localSharedDirPath}:/opt/blackduck/blackduck-imageinspector/shared --name blackduck-imageinspector-alpine blackducksoftware/blackduck-imageinspector-alpine:${imageInspectorVersion} java -jar /opt/blackduck/blackduck-imageinspector/blackduck-imageinspector.jar --server.port=8081 --current.linux.distro=alpine --inspector.url.alpine=http://localhost:9000 --inspector.url.centos=http://localhost:9001 --inspector.url.ubuntu=http://localhost:9002
 fi
 
+##############################
+centosServiceIsUp=false
+
+successMsgCount=$(curl http://localhost:9001/health | grep "\"status\":\"UP\"" | wc -l)
+if [ "$successMsgCount" -eq "1" ]; then
+	echo "The centos service is up"
+	centosServiceIsUp=true
+else
+	# Start the image inspector service for centos on port 9001
+	docker run -d --user 1001 -p 9001:8081 --label "app=blackduck-imageinspector" --label="os=UBUNTU" -v ${localSharedDirPath}:/opt/blackduck/blackduck-imageinspector/shared --name blackduck-imageinspector-centos blackducksoftware/blackduck-imageinspector-centos:${imageInspectorVersion} java -jar /opt/blackduck/blackduck-imageinspector/blackduck-imageinspector.jar --server.port=8081 --current.linux.distro=centos --inspector.url.alpine=http://localhost:9000 --inspector.url.centos=http://localhost:9001 --inspector.url.ubuntu=http://localhost:9002
+fi
+
+##############################
+ubuntuServiceIsUp=false
+
+successMsgCount=$(curl http://localhost:9002/health | grep "\"status\":\"UP\"" | wc -l)
+if [ "$successMsgCount" -eq "1" ]; then
+	echo "The ubuntu service is up"
+	ubuntuServiceIsUp=true
+else
+	# Start the image inspector service for ubuntu on port 9002
+	docker run -d --user 1001 -p 9002:8081 --label "app=blackduck-imageinspector" --label="os=CENTOS" -v ${localSharedDirPath}:/opt/blackduck/blackduck-imageinspector/shared --name blackduck-imageinspector-ubuntu blackducksoftware/blackduck-imageinspector-ubuntu:${imageInspectorVersion} java -jar /opt/blackduck/blackduck-imageinspector/blackduck-imageinspector.jar --server.port=8081 --current.linux.distro=ubuntu --inspector.url.alpine=http://localhost:9000 --inspector.url.centos=http://localhost:9001 --inspector.url.ubuntu=http://localhost:9002
+fi
+#############################
+# Wait until imageinspector services are ready
+#############################
 while [ "$alpineServiceIsUp" == "false" ]; do
 	successMsgCount=$(curl http://localhost:9000/health | grep "\"status\":\"UP\"" | wc -l)
 	if [ "$successMsgCount" -eq "1" ]; then
-		echo "The service is up"
+		echo "The alpine service is up"
 		alpineServiceIsUp=true
 		break
 	fi
-	echo "The service is not up yet"
+	echo "The alpine service is not up yet"
 	sleep 15
 done
-
-docker save -o ${localSharedDirPath}/target/alpine1.tar alpine:latest
-cp ${localSharedDirPath}/target/alpine1.tar ${localSharedDirPath}/target/alpine2.tar
-cp ${localSharedDirPath}/target/alpine1.tar ${localSharedDirPath}/target/alpine3.tar
-chmod a+r ${localSharedDirPath}/target/alpine1.tar
-chmod a+r ${localSharedDirPath}/target/alpine2.tar
-chmod a+r ${localSharedDirPath}/target/alpine3.tar
+while [ "$centosServiceIsUp" == "false" ]; do
+	successMsgCount=$(curl http://localhost:9001/health | grep "\"status\":\"UP\"" | wc -l)
+	if [ "$successMsgCount" -eq "1" ]; then
+		echo "The centos service is up"
+		centosServiceIsUp=true
+		break
+	fi
+	echo "The centos service is not up yet"
+	sleep 15
+done
+while [ "$ubuntuServiceIsUp" == "false" ]; do
+	successMsgCount=$(curl http://localhost:9002/health | grep "\"status\":\"UP\"" | wc -l)
+	if [ "$successMsgCount" -eq "1" ]; then
+		echo "The ubuntu service is up"
+		ubuntuServiceIsUp=true
+		break
+	fi
+	echo "The ubuntu service is not up yet"
+	sleep 15
+done
+##############################
 
 curl -O https://detect.synopsys.com/detect.sh
 chmod +x detect.sh
 
 # Call detect to call docker inspector
-./detect.sh --blackduck.url=${blackDuckUrl} --blackduck.username=${blackDuckUsername} --blackduck.password=${blackDuckPassword} --blackduck.trust.cert=true --detect.tools.excluded=SIGNATURE_SCAN,POLARIS --detect.docker.tar=${localSharedDirPath}/target/alpine1.tar --detect.docker.passthrough.imageinspector.service.url=http://localhost:9000 --imageinspector.service.distro.default=alpine --detect.docker.passthrough.shared.dir.path.local=${localSharedDirPath} --detect.docker.passthrough.shared.dir.path.imageinspector=/opt/blackduck/blackduck-imageinspector/shared --detect.docker.passthrough.imageinspector.service.start=false --logging.level.com.blackducksoftware.integration=DEBUG --detect.project.name=${projectName} --detect.project.version.name=alpine1 --detect.project.codelocation.prefix=1
+./detect.sh --blackduck.url=${blackDuckUrl} --blackduck.username=${blackDuckUsername} --blackduck.password=${blackDuckPassword} --blackduck.trust.cert=true --detect.tools.excluded=SIGNATURE_SCAN --detect.docker.image=alpine:latest --detect.docker.passthrough.imageinspector.service.url=http://localhost:9000 --imageinspector.service.distro.default=centos --detect.docker.passthrough.shared.dir.path.local=${localSharedDirPath} --detect.docker.passthrough.shared.dir.path.imageinspector=/opt/blackduck/blackduck-imageinspector/shared --detect.docker.passthrough.imageinspector.service.start=false --logging.level.com.synopsys.integration=INFO 
 
-./detect.sh --blackduck.url=${blackDuckUrl} --blackduck.username=${blackDuckUsername} --blackduck.password=${blackDuckPassword} --blackduck.trust.cert=true --detect.tools.excluded=SIGNATURE_SCAN,POLARIS --detect.docker.tar=${localSharedDirPath}/target/alpine2.tar --detect.docker.passthrough.imageinspector.service.url=http://localhost:9000 --imageinspector.service.distro.default=alpine --detect.docker.passthrough.shared.dir.path.local=${localSharedDirPath} --detect.docker.passthrough.shared.dir.path.imageinspector=/opt/blackduck/blackduck-imageinspector/shared --detect.docker.passthrough.imageinspector.service.start=false --logging.level.com.blackducksoftware.integration=DEBUG --detect.project.name=${projectName} --detect.project.version.name=alpine2 --detect.project.codelocation.prefix=2
+./detect.sh --blackduck.url=${blackDuckUrl} --blackduck.username=${blackDuckUsername} --blackduck.password=${blackDuckPassword} --blackduck.trust.cert=true --detect.tools.excluded=SIGNATURE_SCAN --detect.docker.image=centos:latest --detect.docker.passthrough.imageinspector.service.url=http://localhost:9000 --imageinspector.service.distro.default=centos --detect.docker.passthrough.shared.dir.path.local=${localSharedDirPath} --detect.docker.passthrough.shared.dir.path.imageinspector=/opt/blackduck/blackduck-imageinspector/shared --detect.docker.passthrough.imageinspector.service.start=false --logging.level.com.synopsys.integration=INFO 
 
-./detect.sh --blackduck.url=${blackDuckUrl} --blackduck.username=${blackDuckUsername} --blackduck.password=${blackDuckPassword} --blackduck.trust.cert=true --detect.tools.excluded=SIGNATURE_SCAN,POLARIS --detect.docker.tar=${localSharedDirPath}/target/alpine3.tar --detect.docker.passthrough.imageinspector.service.url=http://localhost:9000 --imageinspector.service.distro.default=alpine --detect.docker.passthrough.shared.dir.path.local=${localSharedDirPath} --detect.docker.passthrough.shared.dir.path.imageinspector=/opt/blackduck/blackduck-imageinspector/shared --detect.docker.passthrough.imageinspector.service.start=false --logging.level.com.blackducksoftware.integration=DEBUG --detect.project.name=${projectName} --detect.project.version.name=alpine3 --detect.project.codelocation.prefix=3
+./detect.sh --blackduck.url=${blackDuckUrl} --blackduck.username=${blackDuckUsername} --blackduck.password=${blackDuckPassword} --blackduck.trust.cert=true --detect.tools.excluded=SIGNATURE_SCAN --detect.docker.image=ubuntu:latest --detect.docker.passthrough.imageinspector.service.url=http://localhost:9000 --imageinspector.service.distro.default=centos --detect.docker.passthrough.shared.dir.path.local=${localSharedDirPath} --detect.docker.passthrough.shared.dir.path.imageinspector=/opt/blackduck/blackduck-imageinspector/shared --detect.docker.passthrough.imageinspector.service.start=false --logging.level.com.synopsys.integration=INFO 
