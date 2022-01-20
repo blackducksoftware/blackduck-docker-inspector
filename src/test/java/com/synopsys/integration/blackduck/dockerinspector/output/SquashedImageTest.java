802a1d5c23d32dd98022e1b645b1fc84cde04a65
Steve Billings <billings@synopsys.com>
2022-01-20 16:25:16 -0500

    test: fix squashed image test to be order agnostic

--- src/test/java/com/synopsys/integration/blackduck/dockerinspector/output/SquashedImageTest.java
+++ src/test/java/com/synopsys/integration/blackduck/dockerinspector/output/SquashedImageTest.java
@@ -8,12 +8,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collection;
 
 import com.synopsys.integration.blackduck.imageinspector.api.name.ImageNameResolver;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -27,13 +24,13 @@ import com.synopsys.integration.blackduck.imageinspector.linux.FileOperations;
 import com.synopsys.integration.exception.IntegrationException;
 
 @Tag("integration")
-public class SquashedImageTest {
+class SquashedImageTest {
     private static SquashedImage squashedImage;
     private static DockerClientManager dockerClientManager;
     private static File testWorkingDir;
 
     @BeforeAll
-    public static void setUp() throws IOException {
+    static void setUp() throws IOException {
         testWorkingDir = new File("test/output/squashingTest");
         ImageTarFilename imageTarFilename = new ImageTarFilename();
         FileOperations fileOperations = new FileOperations();
@@ -48,7 +45,7 @@ public class SquashedImageTest {
     }
 
     @Test
-    public void testCreateSquashedImageTarGz() throws IOException, IntegrationException {
+    void testCreateSquashedImageTarGz() throws IOException, IntegrationException {
 
         File targetImageFileSystemTarGz = new File("src/test/resources/test_containerfilesystem.tar.gz");
 
@@ -64,15 +61,6 @@ public class SquashedImageTest {
         unpackedSquashedImageDir.mkdirs();
         CompressedFile.gunZipUnTarFile(squashedImageTarGz, tempTarFile, unpackedSquashedImageDir);
 
-
-        // TODO TEMP
-        Collection<File> filesFound = FileUtils.listFilesAndDirs(unpackedSquashedImageDir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
-        System.out.printf("kkk Contents of %s:\n", unpackedSquashedImageDir.getAbsolutePath());
-        for (File foundFile : filesFound) {
-            System.out.printf("%s (symlink: %b; dir: %b)\n", foundFile.getAbsolutePath(), Files.isSymbolicLink(foundFile.toPath()), foundFile.isDirectory());
-        }
-        ////////////
-
         File manifestFile = new File(unpackedSquashedImageDir, "manifest.json");
         assertTrue(manifestFile.isFile());
 
@@ -86,18 +74,16 @@ public class SquashedImageTest {
         }
         assertNotNull(layerDir);
 
-        // Untar the one layer.tar file in image
-        File layerTar = layerDir.listFiles()[0];
+        // Find the layer.tar file
+        File layerTar = null;
+        for (File imageFile : layerDir.listFiles()) {
+            if (imageFile.getName().endsWith(".tar")) {
+                layerTar = imageFile;
+                break;
+            }
+        }
         File layerUnpackedDir = new File(squashingWorkingDir, "squashedImageLayerUnpacked");
         CompressedFile.unTarFile(layerTar, layerUnpackedDir);
-
-        // TODO TEMP
-        filesFound = FileUtils.listFilesAndDirs(layerUnpackedDir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
-        System.out.printf("kkk Contents of %s:\n", layerUnpackedDir.getAbsolutePath());
-        for (File foundFile : filesFound) {
-            System.out.printf("%s (symlink: %b; dir: %b)\n", foundFile.getAbsolutePath(), Files.isSymbolicLink(foundFile.toPath()), foundFile.isDirectory());
-        }
-        ////////////
 
         // Verify that the symlink made it into the squashed image
         File symLink = new File(layerUnpackedDir, "usr/share/apk/keys/aarch64/alpine-devel@lists.alpinelinux.org-58199dcc.rsa.pub");
@@ -109,7 +95,7 @@ public class SquashedImageTest {
     }
 
     @Test
-    public void testGenerateUniqueImageRepoTag() throws IntegrationException {
+    void testGenerateUniqueImageRepoTag() throws IntegrationException {
         String generatedRepTag = squashedImage.generateUniqueImageRepoTag();
 
         assertTrue(generatedRepTag.startsWith("dockerinspectorsquashed-"));
